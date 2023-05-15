b83b2ae730ce63157e4a5bc181faac46852b3b4b
DimaGit51 <Didvoryanchikov@yandex.ru>
2023-05-15 08:30:33 +0400

    Text

---
+++ ClassWork_02_05_2023/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Program
+{
+    public class TreeNode // Класс «Узел бинарного дерева»
+    {
+        private char info; // информационное поле
+        private TreeNode left; // ссылка на левое поддерево
+        private TreeNode right; // ссылка на правое поддерево
+        public char Info
+        {
+            get { return info; }
+            set { info = value; }
+        }
+        public TreeNode Left
+        {
+            get { return left; }
+            set { left = value; }
+        }
+
+        public TreeNode Right
+        {
+            get { return right; }
+            set { right = value; }
+        }
+        public TreeNode() { } // конструкторы
+        public TreeNode(char info)
+        {
+            Info = info;
+        }
+        public TreeNode(char info, TreeNode left, TreeNode right)
+        {
+            Info = info; Left = left; Right = right;
+        }
+    }
+
+    public class BinaryTree // Класс «Бинарное дерево произвольного вида»
+    {
+        private TreeNode root; // ссылка на корень дерева
+        public TreeNode Root // свойство, открывающее доступ к корню дерева
+        {
+            get { return root; }
+            set { root = value; }
+        }
+        public BinaryTree() // инициализация пустого дерева
+        {
+            root = null;
+        }
+        public void KLP(TreeNode root) // root – ссылка на корень дерева и любого из деревьев
+        {
+            if (root !=null ) // дерево не пусто?
+            { 
+                Console.WriteLine(root.Info );// распечатать информ. поле корневого узла
+                KLP(root.Left ); // обойти левое поддерево в нисходящем порядке
+                KLP(root.Right ); // обойти правое поддерево в нисходящем порядке
+            }
+        }
+        public void LPK(TreeNode root) // root – ссылка на корень дерева и любого из деревьев
+        {
+            if (root != null) // дерево не пусто?
+            { 
+                KLP(root.Left); // обойти левое поддерево в нисходящем порядке
+                KLP(root.Right); // обойти правое поддерево в нисходящем порядке
+                Console.WriteLine(root.Info);// распечатать информ. поле корневого узла
+            }
+        }
+        public void LKP(TreeNode root) // root – ссылка на корень дерева и любого из деревьев
+        {
+            if (root != null) // дерево не пусто?
+            { 
+                LKP(root.Left); // обойти левое поддерево в нисходящем порядке
+                Console.WriteLine(root.Info);// распечатать информ. поле корневого узла
+                LKP(root.Right); // обойти правое поддерево в нисходящем порядке
+            }
+
+        }
+
+
+        public int Recursion(TreeNode root)
+        {
+            int count;
+            if (root == null) count = 0;
+            else count = 1 + Recursion(root.Left) + Recursion(root.Right);
+            return count;
+        }
+        public TreeNode CreateRecursive(int n)
+        {
+            Char x; TreeNode root;
+            if (n == 0) root = null;
+            else
+            {
+
+                Console.Write("Введите элемент = ");
+                while ((!Char.TryParse(Console.ReadLine(), out x)))
+                {
+                    Console.Write("Вы ввели не число ^int^! Введите число: ");
+                }
+                root = new TreeNode(x);
+                root.Left = CreateRecursive(n/2);
+                root.Right = CreateRecursive(n - n / 2 - 1);
+
+            }
+            return root;
+        }
+    }
+    public class Program
+    {
+        static void Main()
+        {
+            BinaryTree T = new BinaryTree();
+            T.Root = T.CreateRecursive(7);
+            T.KLP(T.Root);
+            T.LKP(T.Root);
+            Console.WriteLine(T.Recursion(T.Root));
+        }
+    }
+}
---
+++ ClassWork_02_05_2023_2/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Program
+{
+    public class DTreeNode // Класс «Узел дихотомического дерева»
+    {
+        private char info; // информационное поле
+        private int key; // поле ключа
+        private DTreeNode left; // ссылка на левое поддерево
+        private DTreeNode right; // ссылка на правое поддерево
+        public char Info
+        {
+            get { return info; }
+            set { info = value; }
+        } 
+        public int Key
+        {
+            get { return key; }
+            set { key = value; }
+        }
+        public DTreeNode Left
+        {
+            get { return left; }
+            set { left = value; }
+        }
+        public DTreeNode Right
+        {
+            get { return right; }
+            set { right = value; }
+        }
+        public DTreeNode() { } // конструкторы
+        public DTreeNode(char info, int key)
+        {
+            Info = info; Key = key;
+        }
+        public DTreeNode(char info, int key, DTreeNode left, DTreeNode right)
+        {
+            Info = info; Key = key; Left = left; Right = right;
+        }
+    }
+
+
+    public class DichotomyTree // класс «Дихотомическое дерево»
+    {
+        private DTreeNode root; // ссылка на корень дихотомического дерева
+        public DTreeNode Root // свойство, открывающее доступ к корню дерева
+        {
+            get { return root; }
+            set { root = value; }
+        }
+        public DichotomyTree() // инициалиазция пустого дерева
+        {
+            root = null;
+        }
+        public DTreeNode FindDichotomyTree(DTreeNode root, int k)
+        {
+            DTreeNode p;
+
+            if (root == null) p = null;
+            else
+            {
+                if (k < root.Key) p = FindDichotomyTree(root.Left, k);
+                else if (k > root.Key) p = FindDichotomyTree(root.Right, k);
+                else p = root;
+            }
+
+            return p;
+        }
+        public DTreeNode Ins(DTreeNode root, int k)
+        {
+            char x;
+            if (root == null)
+            {
+                Console.Write("Введите инф.полу ");
+                x = Char.Parse(Console.ReadLine());
+                root = new DTreeNode(x, k, null, null);
+            }
+            else
+            {
+                if (k < root.Key) root.Left = Ins(root.Left, k);
+                else if (k > root.Key) root.Right = Ins(root.Right, k);
+            }
+
+            return root;
+        }
+
+    }
+    public class Program
+    {
+        static void Main()
+        {
+            DichotomyTree T1 = new DichotomyTree();
+            T1.Root = T1.Ins(T1.Root, 100);
+            DTreeNode p = T1.FindDichotomyTree(T1.Root, 10);
+            if (p == null) Console.WriteLine("Узел не найлен");
+
+        }
+    }
+}
